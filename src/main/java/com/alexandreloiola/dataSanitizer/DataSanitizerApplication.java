19e5455da4db52f003621584ac079d159bd11891
Alexandre Loiola <alexandre.loiola.741@gmail.com>
2025-03-02 18:51:20 -0300

    chore: Initialize Project

---
+++ src/main/java/com/alexandreloiola/dataSanitizer/DataSanitizerApplication.java
@@ -0,0 +1,13 @@
+package com.alexandreloiola.dataSanitizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataSanitizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataSanitizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alexandreloiola/dataSanitizer/DataSanitizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexandreloiola.dataSanitizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataSanitizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
